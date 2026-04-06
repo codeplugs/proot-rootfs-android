e4e6af8b39992d7ccc831e2a62f31179e5575d93
CodePlug <44272393+codeplugs@users.noreply.github.com>
2026-04-06 16:53:50 +0700

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -11,15 +11,13 @@ LIB_DIR="$FILES_DIR/lib"
 mkdir -p "$ALPINE_DIR" "$BIN_DIR" "$LIB_DIR"
 
 # =============================
-# 🔥 ENV (DARI JAVA)
+# 🔥 ENV
 # =============================
 
 export PATH="/system/bin:/system/xbin:$BIN_DIR:/sbin:/vendor/bin"
 
 export HOME="/root"
-export TERM="xterm-256color"
-export LANG="C.UTF-8"
-export COLORTERM="truecolor"
+
 
 export BIN="$BIN_DIR"
 export PREFIX="$FILES_DIR"
@@ -34,36 +32,6 @@ PROOT_TMP_DIR="$TMPDIR/proot"
 mkdir -p "$PROOT_TMP_DIR"
 export PROOT_TMP_DIR
 
-# LIB FIX
-export LD_LIBRARY_PATH="$LIB_DIR:/system/lib64:/system/lib"
-
-# LINKER FIX
-if [ -f "/system/bin/linker64" ]; then
-    LINKER="/system/bin/linker64"
-else
-    LINKER="/system/bin/linker"
-fi
-export LINKER
-
-# APP INFO
-export PKG="com.rootfs.android"
-export PKG_PATH="/data/app/com.rootfs.android/base.apk"
-export NATIVE_LIB_DIR="/data/app/~~0Tn4vBdxW8FT4LdF__Ideg==/com.rootfs.android-rDSqyUiIVOZPixdhSG-wyA==/lib/arm64"
-
-export DEBUG="false"
-
-# =============================
-# 🔥 PROOT LOADER
-# =============================
-
-if [ -f "$NATIVE_LIB_DIR/libproot-loader.so" ]; then
-    export PROOT_LOADER="$NATIVE_LIB_DIR/libproot-loader.so"
-fi
-
-
-
-# SECCOMP (optional)
-# export SECCOMP=1
 
 # =============================
 # 🔥 EXTRACT ROOTFS
@@ -92,39 +60,3 @@ for sofile in "$FILES_DIR/"*.so*; do
     fi
 done
 
-# =============================
-# 🔥 PROOT ARGS
-# =============================
-ARGS="--kill-on-exit"
-ARGS="$ARGS -w /root"
-
-ARGS="$ARGS -b /dev"
-ARGS="$ARGS -b /proc"
-ARGS="$ARGS -b /sys"
-ARGS="$ARGS -b /sdcard"
-ARGS="$ARGS -b /storage"
-ARGS="$ARGS -b /data"
-
-# STDIO FIX
-ARGS="$ARGS -b /proc/self/fd:/dev/fd"
-ARGS="$ARGS -b /proc/self/fd/0:/dev/stdin"
-ARGS="$ARGS -b /proc/self/fd/1:/dev/stdout"
-ARGS="$ARGS -b /proc/self/fd/2:/dev/stderr"
-
-# SHM FIX
-mkdir -p "$ALPINE_DIR/tmp"
-chmod 1777 "$ALPINE_DIR/tmp"
-ARGS="$ARGS -b $ALPINE_DIR/tmp:/dev/shm"
-
-# ROOTFS
-ARGS="$ARGS -r $ALPINE_DIR"
-ARGS="$ARGS -0"
-ARGS="$ARGS --link2symlink"
-ARGS="$ARGS --sysvipc"
-
-echo "[*] Starting Alpine..."
-
-# =============================
-# 🔥 EXEC PROOT + LOADER
-# =============================
-exec $LINKER "$BIN_DIR/proot" $ARGS /bin/sh -l
