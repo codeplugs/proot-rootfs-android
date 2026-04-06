93b4bce9dac629df204da943be4c217a3225f1e6
aznoisib <aznoisib@gmail.com>
2026-04-06 14:39:59 +0700

    commit

--- app/src/main/java/com/rootfs/android/MainActivity.java
+++ app/src/main/java/com/rootfs/android/MainActivity.java
@@ -126,135 +126,13 @@ private void logProcessOutput(Process process) {
     }).start();
 }
 
-public void startp(Context context) {
-	
-	
-	
-	 new Thread(() -> {
-        try {
-             String FILES_DIR = context.getFilesDir().getAbsolutePath();
-                String ALPINE_DIR = FILES_DIR + "/alpine";
-                String BIN_DIR = FILES_DIR + "/bin";
-                String LIB_DIR = FILES_DIR + "/lib";
-
-                new File(ALPINE_DIR).mkdirs();
-                new File(BIN_DIR).mkdirs();
-                new File(LIB_DIR).mkdirs();
-
-                String linker = new File("/system/bin/linker64").exists()
-                        ? "/system/bin/linker64"
-                        : "/system/bin/linker";
-
-                String prootPath = BIN_DIR + "/proot";
-
-            // -------------------------------
-            // STEP 1: Setup base packages
-            // -------------------------------
-            List<String> setupCmd = new ArrayList<>();
-            setupCmd.add(linker);
-            setupCmd.add(prootPath);
-            setupCmd.add("-r"); setupCmd.add(ALPINE_DIR);
-            setupCmd.add("-0");
-            setupCmd.add("--link2symlink");
-            setupCmd.add("--sysvipc");
-            setupCmd.add("-w"); setupCmd.add("/root");
-            setupCmd.add("-b"); setupCmd.add("/dev");
-            setupCmd.add("-b"); setupCmd.add("/proc");
-            setupCmd.add("-b"); setupCmd.add("/sys");
-
-            setupCmd.add("/bin/sh");
-            setupCmd.add("-c");
-            //setupCmd.add("apk update && apk add alpine-base"); // install base packages
-			
-            //setupCmd.add("apk update && " + "apk add alpine-base alpine-conf && " + "setup-alpine -f -q");
-            
-			//setupCmd.add("apk update && apk add alpine-base alpine-conf busybox bash coreutils findutils && printf 'alpine\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n' | setup-alpine -f -q");
-			//setupCmd.add("apk update && apk add alpine-base alpine-conf busybox bash coreutils findutils && echo -e 'alpine\\nno\\nnone\\nnone\\nnone\\nnone\\n/var/cache/apk\\n' | setup-alpine -f -q");
-           setupCmd.add("apk update && apk add alpine-base alpine-conf busybox bash coreutils findutils && " +
-             "printf 'alpine\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n' | " +
-             "DEFAULT_DISK=none APKCACHEOPTS='/var/cache/apk' setup-alpine -f -q");
-
-		   log("Starting setup base packages...");
-            ProcessBuilder pbSetup = new ProcessBuilder(setupCmd);
-           
-                Map<String, String> envSetup = pbSetup.environment();
-				envSetup.put("PROOT_DEBUG", "1");
-                //env.put("PATH", "/system/bin:/system/xbin:" + BIN_DIR + ":/sbin:/vendor/bin");
-				//export PATH=/bin:/usr/bin:/sbin:/usr/sbin:$PATH
-				// PATH: supaya semua symlink BusyBox bisa ditemukan
-                envSetup.put("PATH", "/bin:/usr/bin:/sbin:/usr/sbin:" + BIN_DIR + ":/system/bin:/system/xbin");
-                envSetup.put("HOSTNAME", "alpine");
-                envSetup.put("HOME", "/root");
-                envSetup.put("TERM", "xterm-256color");
-                envSetup.put("LANG", "C.UTF-8");
-				//envSetup.put("APKCACHEOPTS=","/var/cache/apk");
-                String TMPDIR = FILES_DIR + "/tmp";
-                //new File(TMPDIR).mkdirs();
-                envSetup.put("TMPDIR", TMPDIR);
-                String PROOT_TMP = TMPDIR + "/proot";
-                new File(PROOT_TMP).mkdirs();
-                envSetup.put("PROOT_TMP_DIR", PROOT_TMP);
-                envSetup.put("LD_LIBRARY_PATH", LIB_DIR + ":/system/lib64:/system/lib");
-
-                envSetup.put("NATIVE_LIB_DIR", context.getApplicationInfo().nativeLibraryDir);
-
-                String loader = context.getApplicationInfo().nativeLibraryDir + "/libproot-loader.so";
-                if (new File(loader).exists()) {
-                    envSetup.put("PROOT_LOADER", loader);
-                }
-
-            Process setupProcess = pbSetup.start();
-            logProcessOutput(setupProcess);
-            setupProcess.waitFor();
-
-            log("Base packages setup completed!");
-
-            // -------------------------------
-            // STEP 2: Run login shell
-            // -------------------------------
-            List<String> loginCmd = new ArrayList<>();
-            loginCmd.add(linker);
-            loginCmd.add(prootPath);
-            loginCmd.add("-r"); loginCmd.add(ALPINE_DIR);
-            loginCmd.add("-0");
-            loginCmd.add("--link2symlink");
-            loginCmd.add("--sysvipc");
-            loginCmd.add("-w"); loginCmd.add("/root");
-            loginCmd.add("-b"); loginCmd.add("/dev");
-            loginCmd.add("-b"); loginCmd.add("/proc");
-            loginCmd.add("-b"); loginCmd.add("/sys");
-            loginCmd.add("-b"); loginCmd.add("/system");
-            loginCmd.add("-b"); loginCmd.add("/vendor");
-            loginCmd.add("-b"); loginCmd.add("/data");
-            loginCmd.add("cat /etc/motd"); // login shell, baca /etc/motd
-
-            log("Starting login shell...");
-
-            ProcessBuilder pbLogin = new ProcessBuilder(loginCmd);
-            Map<String, String> envLogin = pbLogin.environment();
-            envLogin.putAll(envSetup); // sama env seperti setup
-            pbLogin.redirectErrorStream(true);
-            Process loginProcess = pbLogin.start();
-            logProcessOutput(loginProcess);
-            loginProcess.waitFor();
-
-            log("Login shell exited.");
-
-        } catch (Exception e) {
-            log("Fatal error: " + e);
-        }
-    }).start();
-	
-	
-	
-     
-    }
 
 
 
 
 
-  /*public void startp(Context context) {
+
+  public void startProot(Context context) {
         new Thread(() -> {
             try {
                 String FILES_DIR = context.getFilesDir().getAbsolutePath();
@@ -323,8 +201,6 @@ public void startp(Context context) {
                 env.put("PATH", "/bin:/usr/bin:/sbin:/usr/sbin:" + BIN_DIR + ":/system/bin:/system/xbin");
 
                 env.put("HOME", "/root");
-                env.put("TERM", "xterm-256color");
-                env.put("LANG", "C.UTF-8");
                 String TMPDIR = FILES_DIR + "/tmp";
                 //new File(TMPDIR).mkdirs();
                 env.put("TMPDIR", TMPDIR);
@@ -381,7 +257,7 @@ public void startp(Context context) {
                 log("Fatal error"+e);
             }
         }).start();
-    }*/
+    }
 
 
 
@@ -420,7 +296,7 @@ public void startp(Context context) {
             @Override
             public void done() {
                 log("Running script...");
-				 startp(MainActivity.this);
+				 startProot(MainActivity.this);
                 //ShellRunner.run(MainActivity.this, MainActivity.this::log);
             }
 
