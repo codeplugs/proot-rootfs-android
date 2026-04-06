6fcc79766dca8108ca1d17877b8ed35a0209fefa
aznoisib <aznoisib@gmail.com>
2026-04-06 18:57:22 +0700

    commit

--- shgs.sh
+++ shgs.sh
@@ -44,14 +44,53 @@ echo "[*] Extract tai"
 echo "[*] Extract tai"
 
 # =============================
-# 🔥 COPY LIB
+# 🔥 proot
 # =============================
-for sofile in "$FILES_DIR/"*.so*; do
-    dest="$LIB_DIR/$(basename "$sofile")"
-    if [ ! -f "$dest" ]; then
-	    echo "[*] Extract rootfs..."
-        cp "$sofile" "$dest"
-        chmod 644 "$dest"
-    fi
+
+# LINKER
+if [ -f "/system/bin/linker64" ]; then
+    LINKER="/system/bin/linker64"
+else
+    LINKER="/system/bin/linker"
+fi
+
+export LINKER
+
+# Pastikan rootfs ada
+mkdir -p "$ALPINE_DIR"
+mkdir -p "$ALPINE_DIR/tmp"
+chmod 1777 "$ALPINE_DIR/tmp"
+
+# PROOT ARGS
+ARGS="--kill-on-exit"
+ARGS="$ARGS -w /root"
+ARGS="$ARGS -b /dev"
+ARGS="$ARGS -b /proc"
+ARGS="$ARGS -b /sys"
+ARGS="$ARGS -b /sdcard"
+ARGS="$ARGS -b /storage"
+ARGS="$ARGS -b /data"
+ARGS="$ARGS -b $ALPINE_DIR/tmp:/dev/shm"
+ARGS="$ARGS -r $ALPINE_DIR -0 --link2symlink --sysvipc"
+
+echo "[*] Starting Alpine with BusyBox..."
+
+# 🔹 PASTIKAN PATH BUSYBOX BENAR
+BUSYBOX="$ALPINE_DIR/bin/busybox"
+if [ ! -f "$BUSYBOX" ]; then
+    echo "[!] BusyBox not found at $BUSYBOX"
+    exit 1
+fi
+
+# 🔹 EXEC PROOT + BUSYBOX SH
+exec "$BIN_DIR/proot" $ARGS "$BUSYBOX" sh
+
+
+
+
+
+
+
+
 done
 
